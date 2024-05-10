971c2e1c987f03d480be0e1df575d2e6cc2752f5
Oleg <oleg.goshovskyy@gmail.com>
2024-05-10 16:30:12 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Pong");
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
