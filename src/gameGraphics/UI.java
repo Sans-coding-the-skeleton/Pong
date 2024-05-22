4e002fff0729160ea896b1a2913b164bfe9e3504
oleg <oleg.goshovskyy@gmail.com>
2024-05-22 11:16:02 +0200

    Added custom font

--- src/gameGraphics/UI.java
+++ src/gameGraphics/UI.java
@@ -3,19 +3,26 @@ package gameGraphics;
 import entity.Ball;
 
 import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class UI {
-    // TODO Custom Font
     GamePanel gp;
-    Font Arial_40;
+    Font pressStartRegular;
 
     public UI(GamePanel gp) {
         this.gp = gp;
-        Arial_40 = new Font("Arial", Font.PLAIN, 40);
+        try {
+            InputStream stream = ClassLoader.getSystemClassLoader().getResourceAsStream("press-start-font/PressStartRegular-ay8E.ttf");
+            assert stream != null;
+            pressStartRegular = Font.createFont(Font.TRUETYPE_FONT, stream).deriveFont(48f);
+        } catch (IOException | FontFormatException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void draw(Graphics2D g2, Ball ball){
-        g2.setFont(Arial_40);
+        g2.setFont(pressStartRegular);
         g2.setColor(Color.WHITE);
         g2.drawString(String.valueOf(ball.getLeftScore()), 300, 50);
         g2.setColor(Color.WHITE);
